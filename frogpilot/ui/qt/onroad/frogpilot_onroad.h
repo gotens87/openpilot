phanja/cobpilot

Display the FPS in the screen's border

frogpilot/ui/qt/onroad/frogpilot_onroad.h
@@ -10,6 +10,8 @@ public:
 
   void updateState(const UIState &s, const FrogPilotUIState &fs);
 
+  double fps;
+
   FrogPilotUIScene frogpilot_scene;
 
   QColor bg;
@@ -18,9 +20,14 @@ public:
 
 private:
   void paintEvent(QPaintEvent *event);
+  void paintFPS(QPainter &p);
   void resizeEvent(QResizeEvent *event);
+
+  bool showFPS;
 
   QRect rect;
 
   QRegion marginRegion;
+
+  QString fpsDisplayString;
 };

gotens87/openpilot

frogpilot/ui/qt/onroad/frogpilot_onroad.h
@@ -10,6 +10,8 @@ public:
 
   void updateState(const UIState &s, const FrogPilotUIState &fs);
 
+  float fps;
+
   FrogPilotUIScene frogpilot_scene;
 
   QColor bg;
@@ -18,9 +20,14 @@ public:
 
 private:
   void paintEvent(QPaintEvent *event);
+  void paintFPS(QPainter &p);
   void resizeEvent(QResizeEvent *event);
+
+  bool showFPS;
 
   QRect rect;
 
   QRegion marginRegion;
+
+  QString fpsDisplayString;
 };